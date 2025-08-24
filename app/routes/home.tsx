2eca5c1806e722b49d60636a60050b6a676ac0b0
Your Name <ravendrapratapsin78@gmail.com>
2025-08-24 21:09:56 +0530

    Today final commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,27 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeRadar" },
+    { name: "Analyzer", content: "Your Job Assistant for your Dream Job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return(
+  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+  <section className="main-section">
+    <div className="page-heading">
+      <h1>Track Your Application and Resume Ratings</h1>
+      <h2> Review and Check AI-powered Feedback</h2>
+    </div >
+
+  </section>
+
+  </main>
+  );
+  
 }
