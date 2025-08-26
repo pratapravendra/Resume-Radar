5ee0322c0a579dd3916168d1aa8377f212ddf82a
Your Name <ravendrapratapsin78@gmail.com>
2025-08-26 14:58:06 +0530

    Added navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,15 @@
+import {Link} from "react-router";
 const Navbar = () => {
   return (
-    <div>
-      <h3>Hello</h3>
-    </div>
+    <nav className="navbar">
+      <Link to="/">
+      <p className="text-2xl font-bold text-gradient">
+        Resume-Radar
+      </p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">Upload-Here</Link>
+
+    </nav>
   )
 }
 
